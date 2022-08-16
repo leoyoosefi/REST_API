b59aa1f7ffd52290d43307c4595fa9d30e3bd9c6
Leo Yoosefi <leoyoosefi@gmail.com>
2022-08-16 13:54:10 +0200

    injection through controller

--- src/main/java/se/lexicon/rest_api/controller/RoleController.java
+++ src/main/java/se/lexicon/rest_api/controller/RoleController.java
@@ -1,10 +1,15 @@
 package se.lexicon.rest_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.lexicon.rest_api.model.entity.Role;
 import se.lexicon.rest_api.repository.RoleRepo;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/role")
@@ -17,5 +22,10 @@ public class RoleController {
         this.roleRepo = roleRepo;
     }
 
-    @GetMapping("")
+    @GetMapping("/")
+    public ResponseEntity<List<Role>> getAllRoles(){
+
+        List<Role> roleList = roleRepo.findAll();
+        return ResponseEntity.ok(roleList);
+    }
 }
